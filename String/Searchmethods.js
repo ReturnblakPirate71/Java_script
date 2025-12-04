389455fa063ec1424074fbf47c0f263d228d3716
ReturnblakPirate71 <rafialsakif92640@example.com>
2025-12-05 00:11:42 +0600

    Initial commit with local project

---
+++ String/Searchmethods.js
@@ -0,0 +1,11 @@
+let serverName = `KashiMpurkirqoppqjwyq`
+
+console.log(serverName)
+
+console.log(typeof (serverName))
+let length = serverName.length
+console.log(serverName.charAt(length-1))
+let codeOfchar = serverName.charCodeAt(length-4)
+console.log(codeOfchar)
+
+console.log(serverName.lastIndexOf(`i`))
